da6e30e34a48e10b03fc679bd41eef305fc821a2
Jose Guadalupe Mendoza Vallejo <mendozavallejojoseguadalupe@gmail.com>
2026-09-11 12:49:10 -0600

    Hola mundo y otros ejercicios

---
+++ holamundo.py
@@ -0,0 +1,9 @@
+print("Hola Mundo")
+nombre1 = "Michel"
+print("La persona que se llama " + nombre1 + " Pinche Huevona ")
+
+nombre2 = input("Ingresa tu nombre: ")
+print("Hola, " + nombre2 + " Ponte a trabajar pinche huevona")
+
+suma = 5 + 3
+print("La suma de 5 + 3 es: " + str(suma))
