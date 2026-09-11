1adbb487317582830fda3c3202d00e58735af480
Jose Guadalupe Mendoza Vallejo <mendozavallejojoseguadalupe@gmail.com>
2026-09-11 12:58:00 -0600

    Quemador de Gays

---
+++ quienesgay.py
@@ -0,0 +1,7 @@
+personaGay = input("Ingresa tu nombre: ")
+if personaGay == "Michel":
+    print("¡Eres gay, Michel!")
+elif personaGay == "Cristian":
+    print("¡Eres gay, Cristian!")
+else:
+    print("¡No eres gay, " + personaGay + "!")
